2eaa29066f85850d03282ecdf58e1ae8ac09bdb3
cayasso <cayasso@gmail.com>
2017-11-30 22:10:40 -0600

    fixing env variable

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  CLIENT_ID: process.env.GPAYMENTS_CLIENT_SECRET,
+  CLIENT_ID: process.env.GPAYMENTS_CLIENT_ID,
   CLIENT_SECRET: process.env.GPAYMENTS_CLIENT_SECRET,
   API_URL: process.env.GPAYMENTS_API_URL || 'https://api.payments.4geeks.io'
 }
